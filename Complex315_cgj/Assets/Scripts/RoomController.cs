acb68d2e8f99c22d32803304ec2a8dc8e17b0cf3
caygilpaul <pfcaygill@gmail.com>
2019-08-25 16:55:31 +1200

    floor tiles working, npc animation working, sound in

---
+++ Complex315_cgj/Assets/Scripts/RoomController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    public string areaType;
+    void Start()
+    {
+        //when the game starts and not when it wakes, play the track for the room type looped
+        FindObjectOfType<SingletonAudioController>().Play(areaType);
+        //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
